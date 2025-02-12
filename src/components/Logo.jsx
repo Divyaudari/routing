efe67fda8d87d61d082931ea033e1278b976616c
Divyaudari <divyaudari7@gmail.com>
2025-02-12 13:04:33 +0530

    react

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Styles from "./navar.module.css";
+import Image from "./image.png"
+const Logo = () => {
+  return (
+    <div className={Styles.logoBlock}>
+        <Image/>
+    </div>
+  )
+}
+
+export default Logo
---
+++ src/components/Menu.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div className={StyleSheet.menuBlock}>
+        <ul>
+            <li><a href=''></a></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            
+            
+            
+            </ul></div>
+  )
+}
+
+export default Menu
