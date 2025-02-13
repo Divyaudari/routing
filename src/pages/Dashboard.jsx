bb9d71936a0a4a7e0a086d7a38b86adacd23e1ac
Divyaudari <divyaudari7@gmail.com>
2025-02-13 13:02:54 +0530

    react

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
-
+import { useState } from 'react'
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
+const [username, setUsername] = useState('');
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+const [gender, setGender] = useState('');
+const [address, setaddress] = useState('');
+const [file, setFile] = useState('');  
+
+
+
+
+return (
+    <div>
+      <form action="">
+        <input type='text' placeholder='username'/> <br/>
+        <input type='email' placeholder='Email'/> <br/>
+        <input type='password' placeholder='password'/> <br/>
+
+        <input type='radio' placeholder='gender'/><label htmlFor="" >Male</label>
+        <input type='radio' placeholder='gender'/><label htmlFor="" >Female</label>
+        <input type='radio' placeholder='gender'/><label htmlFor="" >others</label>
+        <br/>
+        <textarea name="" id="add"
+        value={address}
+        ></textarea>
+
+      </form>
+      
+    </div>
   )
 }
 
